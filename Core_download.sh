f07989872acf2567e1f45d8a249c1b3c0a164f6e
mbugaiov <maxim.bugaiov@softheme.com>
2017-06-08 12:28:24 +0300

    Included retriew of the valid builds of the Core

--- Core_download.sh
+++ Core_download.sh
@@ -6,8 +6,19 @@ link="https://tc.appassure.com/viewType.html?buildTypeId=AppAssure_Windows_Devel
 wget --no-check-certificate --user=mbugaiov --password=123asdQQ\!@#$ $link -O "$FILE" > /dev/null 2>&1
 id=`cat TC.log | grep "This build" | grep -E -o "buildId=[[:digit:]]*" | sort -n -r | cut -d "=" -f2 | sed -n 1p`
 build=`cat $FILE | grep -E -o "#develop-7.0.0.[[:digit:]]*" | cut -d "." -f4 | sed -n 1p`
-echo "Downloading of the $branch.$build Core build has been started."
+echo "Retrieving of the $branch.$build Core"
 rm -r $FILE # cleanup html page, since it is not needed anymore
 build_link="https://tc.appassure.com/repository/download/AppAssure_Windows_Develop_FullBuild/$id:id/installers/Core-X64-$branch.$build.exe"
+error_code=`wget --no-check-certificate --user=mbugaiov --password=123asdQQ\!@#$ -q --spider $build_link; echo $?`
+
+while [ $error_code != 0 ]
+do
+	build=$(($build -1))
+	build_link="https://tc.appassure.com/repository/download/AppAssure_Windows_Develop_FullBuild/$id:id/installers/Core-X64-$branch.$build.exe"
+	error_code=`wget --no-check-certificate --user=mbugaiov --password=123asdQQ\!@#$ -q --spider $build_link; echo $?`
+	echo "Retrieving of the $branch.$build Core"
+done
+
+echo "Retrieving of the $branch.$build Core build has been completed. $branch.$build Core starts to be downloaded."
 aria2c -x 16 --http-user=$username --http-passwd=123asdQQ\!@#$ $build_link
 
