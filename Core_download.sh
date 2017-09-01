bf6c7a75308ac37144ed2594cf5bf2cbc98421a9
mbugaiov <maxim.bugaiov@softheme.com>
2017-09-01 11:11:29 +0300

    Fixed new Core_download.sh script to use continues branch

--- Core_download.sh
+++ Core_download.sh
@@ -1,21 +1,24 @@
 #!/bin/bash
 FILE="TC.log"
 username="mbugaiov"
-branch="7.0.0"
-link="https://tc.appassure.com/viewType.html?buildTypeId=AppAssure_Windows_Develop_FullBuild"
+branch="7.1.0"
+link="https://tc.appassure.com/viewType.html?buildTypeId=AppAssure_Windows_Develop20_FullBuild"
 wget --no-check-certificate --user=mbugaiov --password=123asdQQ\!@#$ $link -O "$FILE" > /dev/null 2>&1
-id=`cat TC.log | grep "This build" | grep -E -o "buildId=[[:digit:]]*" | sort -n -r | cut -d "=" -f2 | sed -n 1p`
-build=`cat $FILE | grep -E -o "#develop-7.0.0.[[:digit:]]*" | cut -d "." -f4 | sed -n 1p`
+id=`cat TC.log | grep "build:" | grep -E -o "buildId=[[:digit:]]*" | sort -n -r | cut -d "=" -f2 | sed -n 1p`
+build=`cat $FILE | grep -E -o "#develop-7.1.0.[[:digit:]]*" | cut -d "." -f4 | sed -n 1p`
 echo "Retrieving of the $branch.$build Core"
-rm -r $FILE # cleanup html page, since it is not needed anymore
-build_link="https://tc.appassure.com/repository/download/AppAssure_Windows_Develop_FullBuild/$id:id/installers/Core-X64-$branch.$build.exe"
+#rm -r $FILE # cleanup html page, since it is not needed anymore
+build_link="https://tc.appassure.com/repository/download/AppAssure_Windows_Develop20_FullBuild/$id:id/installers/Core-X64-$branch.$build.exe"
 error_code=`wget --no-check-certificate --user=mbugaiov --password=123asdQQ\!@#$ -q --spider $build_link; echo $?`
+
+echo $build_link
 
 while [ $error_code != 0 ]
 do
 	build=$(($build -1))
-	build_link="https://tc.appassure.com/repository/download/AppAssure_Windows_Develop_FullBuild/$id:id/installers/Core-X64-$branch.$build.exe"
+	build_link="https://tc.appassure.com/repository/download/AppAssure_Windows_Develop20_FullBuild/$id:id/installers/Core-X64-$branch.$build.exe"
 	error_code=`wget --no-check-certificate --user=mbugaiov --password=123asdQQ\!@#$ -q --spider $build_link; echo $?`
+	echo $build_link
 	echo "Retrieving of the $branch.$build Core"
 done
 
