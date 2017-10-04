866c5994bbdbbfc5221ad7497e830c890f7661db
mbugaiov <maxim.bugaiov@softheme.com>
2017-10-04 13:38:59 +0300

    Added script for downloading LiveCD for the Linux Agent

---
+++ Livedvd_download.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+FILE="TC.log"
+username="mbugaiov"
+branch="7.1.0"
+link="https://tc.appassure.com/viewType.html?buildTypeId=AppAssure_Linux_RebrandedDevelop_AgentBuilds_Debian8x64"
+wget --no-check-certificate --user=mbugaiov --password=123asdQQ\!@#$ $link -O "$FILE" > /dev/null 2>&1
+id=`cat TC.log | grep "build:" | grep -E -o "buildId=[[:digit:]]*" | sort -n -r | cut -d "=" -f2 | sed -n 2p`
+build=`cat $FILE | grep -E -o "#develop-7.1.0.[[:digit:]]*" | cut -d "." -f4 | sed -n 1p`
+echo "Retrieving of the $branch.$build LiveDVD"
+#rm -r $FILE # cleanup html page, since it is not needed anymore
+function build {
+	build_link="https://tc.appassure.com/repository/download/AppAssure_Linux_RebrandedDevelop_AgentBuilds_Debian8x64/$id:id/rapidrecovery-livedvd-$branch.$build.iso"
+}
+build
+error_code=`wget --no-check-certificate --user=mbugaiov --password=123asdQQ\!@#$ -q --spider $build_link; echo $?`
+
+echo $build_link
+
+while [ $error_code != 0 ]
+do
+	build=$(($build -1))
+	build
+	error_code=`wget --no-check-certificate --user=mbugaiov --password=123asdQQ\!@#$ -q --spider $build_link; echo $?`
+	echo $build_link
+	echo "Retrieving of the $branch.$build LiveDVD"
+done
+
+echo "Retrieving of the $branch.$build LIVEDVD iso has been completed. $branch.$build LiveDVD starts to be downloaded."
+aria2c -x 16 --http-user=$username --http-passwd=123asdQQ\!@#$ $build_link --out="Livedvd.iso"
+
