9efd1f8919cd9e27b929e197576be66abab0987d
mbugaiov <maxim.bugaiov@softheme.com>
2018-05-04 17:07:37 +0300

    Fix the name

--- shutdown.sh
+++ shutdown.sh
@@ -24,26 +24,33 @@ case $1 in
 esac
 
 <<COMMENT
-The oracle.txt file is the file, where credentials to the oracle databases
+The oracle.conf file is the file, where credentials to the oracle databases
 are set.
 Example of the file:
 # The following description:
 # databas name : user to login : password for the user
 
-DATABASE=XE:sys:123asdQ,TEST:123:qwe 
+DATABASE=XE:sys:123,TEST:123:qwe 
 
 Where:
  'DATABASE' is the string variable.
  'XE'      - the ORACLE_SID   (`ps -ef | grep -e _pmon_ | head -n 1 | awk '{print $8}' | cut -d'_' -f3`);
- 'sys'     - login to the sqlplus;
- '123asdQ' - Password for the login user;
+ 'sys'     - login to the specified database instance;
+ '123'     - Password for the login user to the specified database instance;
  ','       - Separator between databases;
 
 COMMENT
 
-source oracle.txt
+if [ -f oracle.conf ]; then
+    source oracle.conf
+else
+    echo "The configuration file oracle.conf is not found. This file needs to be in the same folder, where"
+    echo "running snap_oracleDB.sh is."
+    exit 1
+fi
 
 echo $DATABASE
+sqlplus=`which sqlplus`
 
 IFS_OLD=$IFS
 IFS=","; databases=( $DATABASE )
@@ -51,10 +58,10 @@ IFS=$IFS_OLD
 
 archivelog_mode () {
 	export ORACLE_SID=$3
-	sqlplus /nolog << EOF 2>&1
+	$sqlplus /nolog << EOF 2>&1
 	connect $1/$2 as sysdba
-	WHENEVER SQLERROR EXIT SQL.SQLCODE
-	shutdown immediate;
+	WHENEVER SQLERROR EXIT SQL.SQLCODE;
+        shutdown immediate;
 	startup mount;
 	alter database archivelog;
 	alter database open;
@@ -66,10 +73,10 @@ EOF
 
 noarchivelog_mode () {
         export ORACLE_SID=$3
-        sqlplus /nolog << EOF 2>&1
+        $sqlplus /nolog << EOF 2>&1
         connect $1/$2 as sysdba
-        WHENEVER SQLERROR EXIT SQL.SQLCODE
-	shutdown immediate;
+        WHENEVER SQLERROR EXIT SQL.SQLCODE;
+        shutdown immediate;
         startup mount;
         alter database noarchivelog;
         alter database open;
@@ -81,54 +88,54 @@ EOF
 
 begin_backup () {
         export ORACLE_SID=$3
-        sqlplus /nolog << EOF 2>&1
+        $sqlplus /nolog << EOF 2>&1
         connect $1/$2 as sysdba
-        WHENEVER SQLERROR EXIT SQL.SQLCODE
+        WHENEVER SQLERROR EXIT SQL.SQLCODE;
         alter database begin backup;
-	select * from v\$database;
+        select * from v\$database;
         select name, log_mode, open_mode from v\$database;
         SELECT INSTANCE_NAME, STATUS, DATABASE_STATUS FROM V\$INSTANCE;
-	exit;
+        exit;
 EOF
 
 }
 
 end_backup () {
         export ORACLE_SID=$3
-        sqlplus /nolog << EOF 2>&1
+        $sqlplus /nolog << EOF 2>&1
         connect $1/$2 as sysdba
-        WHENEVER SQLERROR EXIT SQL.SQLCODE
+        WHENEVER SQLERROR EXIT SQL.SQLCODE;
         alter database end backup;
-	select * from v\$backup;
-	select name, log_mode, open_mode from v\$database;
-	SELECT INSTANCE_NAME, STATUS, DATABASE_STATUS FROM V\$INSTANCE;
-	exit;
+        select * from v\$backup;
+        select name, log_mode, open_mode from v\$database;
+        SELECT INSTANCE_NAME, STATUS, DATABASE_STATUS FROM V\$INSTANCE;
+        exit;
 EOF
 
 }
 
 for i in ${databases[@]}; do
-	_database=`echo $i | cut -d':' -f1`
+        _database=`echo $i | cut -d':' -f1`
         echo "The database name is : $_database"
-	login=`echo $i | cut -d':' -f2`
+        login=`echo $i | cut -d':' -f2`
         echo "The database login is : $login"
-	password=`echo $i | cut -d':' -f3`
+        password=`echo $i | cut -d':' -f3`
         echo "The database password is : $password"
-	if [[ $mode == "on" ]]; then
+        if [[ $mode == "on" ]]; then
 		archivelog_mode $login $password $_database || exit_code=$?
-	elif [[ $mode == "off" ]]; then
+        elif [[ $mode == "off" ]]; then
 		noarchivelog_mode $login $password $_database || exit_code=$?
-	elif [[ $mode == "begin" ]]; then
+        elif [[ $mode == "begin" ]]; then
 		begin_backup $login $password $_database || exit_code=$?
-	elif [[ $mode == "end" ]]; then
+        elif [[ $mode == "end" ]]; then
 		end_backup $login $password $_database || exit_code=$?
-	else
+        else
 		echo "No known mode selected. Exit 2"
 		exit 2
-	fi
-	if [ "$exit_code" ]; then
+        fi
+        if [ "$exit_code" ]; then
 		echo "Script has failed with the $exit_code error code"
 		exit $exit_code
-	fi
+        fi
 done
 
